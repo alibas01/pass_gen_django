6d2598b0adbe6898f3ec7cc64da42d438c1e566b
Ali Bas <alibas01@gmail.com>
2021-06-06 15:22:26 -0400

    new input word feature

--- pass_gen/views.py
+++ pass_gen/views.py
@@ -17,7 +17,8 @@ def password(request):
     nums = list('1234567890')
     #exception = copy.deepcopy(nums)
     word = copy.deepcopy(lowercase)
-    passthe=random.choice(lowercase)
+    passthe=""
+    reference = request.GET.get('reference', "")
 
     if request.GET.get('uppercase'):
         word.extend(uppercase)
@@ -31,14 +32,51 @@ def password(request):
     else:
         passthe=""
 
+    def mix(stri):
+        result = ''
+        counter = 0
+        mixwords = ['A','a','e','E','o','O','l','L','s','S','B', 'i', 'I']
+        for char in stri:
+            if char not in mixwords and len(result)<4:
+                result += char
+            elif char not in mixwords and counter/len(result)<0.4:
+                result += random.choice(word)
+            elif char not in mixwords:
+                result += char                
+            else:
+                if char=='A':
+                    result += "4"
+                    counter +=1
+                if char=='a':
+                    result += "@"
+                    counter +=1
+                if char=='B':
+                    result += "8"
+                    counter +=1
+                if char=='e' or char=='E':
+                    result += "3"
+                    counter +=1
+                if char=='o' or char=='O':
+                    result += "0"
+                    counter +=1
+                if char=='l' or char=='L':
+                    result += "1"
+                    counter +=1
+                if char=='s' or char=='S':
+                    result += "$"
+                    counter +=1
+                if char=='i' or char=='I':
+                    result += "!"
+                    counter +=1
+        return result
 
-    for x in range(length):
-        passthe += random.choice(word)
-
+    if len(reference)==0:
+        for x in range(length):
+            passthe += random.choice(word)
+    else:
+        passthe=mix(reference)
 
     return render(request, 'pass.html', {'password': passthe, 'author': 'Ali Bas'})
-
-
 
 def about(request):
     return render(request, 'about.html', {'author': 'Ali Bas'})
